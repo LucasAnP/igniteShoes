24a540b19a9f7ba5b404ba5bdd6408d302562dfd
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-09-04 12:39:43 -0300

    Creation of tags

--- App.tsx
+++ App.tsx
@@ -13,11 +13,18 @@ import { THEME } from "./src/theme";
 import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
+import { TagUserEmailCreate } from "./src/notifications/notificationsTags";
 
 OneSignal.setAppId("f0f31828-df5a-4c02-834a-4779c0713787");
 
+OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+  console.log(response);
+});
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  TagUserEmailCreate("lucas.antonio199@outlook.com");
 
   return (
     <NativeBaseProvider theme={THEME}>
